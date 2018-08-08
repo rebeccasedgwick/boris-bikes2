e5a71b5f8c1e54c12fb9bcfbea83620f31b6a0b8
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-08-08 15:26:02 +0100

    Modified tests and classes, added release_bike and working methods

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,6 +1,13 @@
 require "docking_station"
+require "bike"
 describe DockingStation do
   it "releases a bike" do
     expect(subject).to respond_to :release_bike
   end
+
+  it "release bike gets a working bike" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 end
