b8d7281f128c5f9b7d998dedda59c96d4c1b1896
Becky Sedgwick <becky@Beckys-MacBook-Pro.local>
2018-08-08 17:17:13 +0100

    Dock bike tests, methods & comments

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,4 +3,20 @@ class DockingStation
   def release_bike
     Bike.new
   end
+
+  def dock_bike(bike)
+# ideally you'd want to check that the bike is actually a Bike object
+
+     #(bike) << this needs to have a parameter
+# needs to put a bike in an array
+  end
+
+# def has_bikes?
+#   needs to read the array and return if it has bikes or not
+#   this is / uses the attr_reader on @array
+# end
+
+# @array  = []
+# this needs to store the bikes and be accessible to other methods
+
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -10,4 +10,12 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
+# Write a unit test for docking a bike at a docking station
+  it "responds to dock_bike(bike)" do
+    bike = Bike.new
+    expect(subject.dock_bike(bike)).to eq nil
+    # currently returns nil (because the method does nothing)
+  end
+
+
 end
