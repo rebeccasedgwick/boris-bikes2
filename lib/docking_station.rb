16fd9679a258f8b36255bc9608342f4cafd9c8ae
Becky Sedgwick <becky@Beckys-MacBook-Pro.local>
2018-08-09 17:29:43 +0100

    Added private methods for full? and empty? to DockingStation objects

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,20 +2,29 @@ require_relative "bike"
 
 class DockingStation
 
-  attr_reader :show_bike
+ attr_reader :show_bike
 
-  def initialize
-    @show_bike = []
-  end
+ def initialize
+   @show_bike = []
+ end
 
-  def release_bike
-    raise 'no bikes to release' if @show_bike.empty?
-    @show_bike.pop
-  end
+ def release_bike
+   raise 'no bikes to release' if empty?
+   @show_bike.pop
+ end
 
-  def dock(bike)
-    raise 'Docking Station is full!' unless @show_bike.count < 20
-    @show_bike << bike
-  end
+ def dock(bike)
+   raise 'Docking Station is full!' if full?
+   @show_bike << bike
+ end
+
+ private
+ def full?
+   @show_bike.count >= 20
+ end
+
+ def empty?
+   @show_bike.empty?
+ end
 
 end
